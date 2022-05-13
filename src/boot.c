6f279767e343abb0a6f03c5b1dffefe3e5b2abf0
Joseph Keane <joseph_keane@live.co.uk>
2022-05-13 12:09:38 +0100

    Start adding RSDP support

--- src/boot.c
+++ src/boot.c
@@ -3,15 +3,31 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdarg.h> 
+#include "types.h"
+
+
 
 EFI_STATUS print(CHAR16 *string) {
     return ST->ConOut->OutputString(ST->ConOut, string);
 }
 
+EFI_STATUS print_char8(CHAR8 *string, size_t length) {
+    #define MAX_STRING_LEN 256
+    if (length > MAX_STRING_LEN) {
+        return print(L"Max string print length exceeded. Use print(CHAR16*) instead.");
+    }
+    CHAR16 string16[MAX_STRING_LEN];
+    for (size_t i = 0; i < length; i++) {
+        string16[i] = string[i];
+    }
+    return ST->ConOut->OutputString(ST->ConOut, string16);
+}
+
 bool are_guids_eq(EFI_GUID guid1, EFI_GUID guid2) {
     size_t size = 16;
-    char *array1 = (char *) &guid1;
-    char *array2 = (char *) &guid2;
+    CHAR8 *array1 = (CHAR8 *) &guid1;
+    CHAR8 *array2 = (CHAR8 *) &guid2;
 
     for (size_t i = 0; i < size; i++) {
         if (array1[i] != array2[i]) return false;
@@ -27,23 +43,13 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
     /* Store the system table for future use in other functions */
     ST = SystemTable;
 
-    for (int I = 0; I < ST->NumberOfTableEntries; I++) {
+    for (UINT8 I = 0; I < ST->NumberOfTableEntries; I++) {
         EFI_CONFIGURATION_TABLE Table = ST->ConfigurationTable[I];
         EFI_GUID ACPI_2_Table = ACPI_20_TABLE_GUID;
 
         if(are_guids_eq(Table.VendorGuid, ACPI_2_Table)) {
-            #define SignatureSize 8
-            char * Signature8 = (char *) Table.VendorTable;
-            CHAR16 Signature16[SignatureSize+3];
-            for (size_t J = 0; J < SignatureSize; J++) {
-                Signature16[J] = Signature8[J];
-            }
-            Signature16[SignatureSize] = '\r';
-            Signature16[SignatureSize+1] = '\n';
-            Signature16[SignatureSize+2] = '\0';
-
-            // Status = print(L"Success!\r\n");
-            Status = print(Signature16);
+            struct RSDP *Rsdp = (struct RSDP *) Table.VendorTable;
+            Status = print_char8(Rsdp->Signature, 8);
             if (EFI_ERROR(Status)) return Status;
         }
     }
