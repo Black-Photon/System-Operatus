8eb447687eff5d05908f9142cf7c884aeb1e14f9
Joseph Keane <joseph_keane@live.co.uk>
2022-05-19 20:30:42 +0100

    Start searching for PCI devices

--- src/boot.c
+++ src/boot.c
@@ -56,6 +56,75 @@ void handle_error(char *string) {
     char c = getchar();
 }
 
+void *get_physical_address(uint8_t bus, uint8_t device, uint8_t function, uint8_t *starting_address, uint8_t starting_bus) {
+    return (void *) (starting_address + ((bus - starting_bus) << 20 | device << 15 | function << 12));
+}
+
+uint16_t get_vendor_id(uint16_t *physical_address) {   
+    return physical_address[1];
+}
+
+uint8_t get_header_type(uint8_t *physical_address) {
+    return physical_address[13];
+}
+
+uint8_t get_base_class(uint8_t *physical_address) {
+
+}
+
+uint8_t get_sub_class(uint8_t *physical_address) {
+
+}
+
+uint8_t get_secondary_bus(uint8_t *physical_address) {
+
+}
+
+// void check_function(uint8_t bus, uint8_t device, uint8_t function) {
+//     uint8_t base_class;
+//     uint8_t sub_class;
+//     uint8_t secondary_bus;
+
+//     base_class = get_base_class(bus, device, function);
+//     sub_class = get_sub_class(bus, device, function);
+//     if ((base_class == 0x6) && (sub_class == 0x4)) {
+//         secondary_bus = get_secondary_bus(bus, device, function);
+//         check_bus(secondary_bus);
+//     }
+// }
+
+void check_device(uint8_t bus, uint8_t device, uint8_t *starting_address, uint8_t starting_bus) {
+    uint8_t function = 0;
+ 
+    void *physical_address = get_physical_address(bus, device, function, starting_address, starting_bus);
+
+    uint16_t vendor_id = get_vendor_id((uint16_t *) physical_address);
+    if (vendor_id == 0xFFFF) return; // Device doesn't exist
+    printf("ID = %x\n", vendor_id);
+    // check_function(bus, device, function);
+    // uint8_t header_type = get_header_type((uint8_t *) physical_address);
+    // if ((header_type & 0x80) != 0) {
+    //     // It's a multi-function device, so check remaining functions
+    //     for (function = 1; function < 8; function++) {
+    //         physical_address = get_physical_address(bus, device, function, starting_address, starting_address);
+    //         if (get_vendor_id(physical_address) != 0xFFFF) {
+    //             check_function(bus, device, function);
+    //         }
+    //     }
+    // }
+}
+
+void check_all_buses(uint8_t *starting_address, uint8_t starting_bus) {
+    uint16_t bus;
+    uint8_t device;
+
+    for (bus = 0; bus < 256; bus++) {
+        for (device = 0; device < 32; device++) {
+            check_device(bus, device, starting_address, starting_bus);
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     rsdp_t *rsdp = get_rsdp_table();
@@ -132,7 +201,10 @@ int main(int argc, char **argv)
         printf("- Base address: %lx\n", entry->base_address);
         printf("- PCI segment group number: %d\n", entry->pci_segment_group_number);
         printf("- Start bus number: %d\n", entry->start_bus_no);
-        printf("- End bus number: %d\n", entry->end_bus_no);        
+        printf("- End bus number: %d\n", entry->end_bus_no);
+
+        // printf("Contents = %d\n", *((uint32_t *) entry->base_address));
+        check_all_buses((uint8_t *) entry->base_address, entry->start_bus_no);
     }
 
 
